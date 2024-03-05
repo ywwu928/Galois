25358c7cd0af7b5968f5fd515d5911fadc49ea98
ywwu928 <ywwu928@utexas.edu>
2024-03-05 10:52:11 -0600

    instrumentation for ideal caching

--- lonestar/analytics/distributed/instrumentation/instrument_ideal.h
+++ lonestar/analytics/distributed/instrumentation/instrument_ideal.h
@@ -6,6 +6,10 @@ constexpr int CACHE_BOUND = 50;
 constexpr int CACHE_SAMPLES = 10;
 constexpr int CACHE_STEP = CACHE_BOUND / CACHE_SAMPLES;
 
+bool sortAccess (const std::vector<uint64_t>& v1, const std::vector<uint64_t>& v2) { // descending order
+    return v1[3] > v2[3]; // index 3 is # of accesses
+}
+
 template <typename Graph>
 struct Instrument {
 #if GALOIS_INSTRUMENT
@@ -13,9 +17,9 @@ struct Instrument {
   uint64_t hostID;
   uint64_t numHosts;
 
-  // size of total nodes; cache level for each mirror (lower level higher
-  // in-degree)
-  std::vector<int> node_cache_level;
+  // size of total mirror nodes; read / write accesses for each mirror
+  std::vector<std::vector<uint64_t>> node_access;
+  std::shared_ptr<galois::substrate::SimpleLock> vector_lock;
 
   std::unique_ptr<galois::DGAccumulator<uint64_t>> local_read_stream;
   std::unique_ptr<galois::DGAccumulator<uint64_t>> local_write_stream;
@@ -42,9 +46,8 @@ struct Instrument {
     graph    = g.get();
     hostID   = hid;
     numHosts = numH;
-
-    // set up final result container
-    node_cache_level.resize(graph->size());
+  
+    vector_lock = std::make_shared<galois::substrate::SimpleLock>();
 
     local_read_stream = std::make_unique<galois::DGAccumulator<uint64_t>>();
     local_write_stream = std::make_unique<galois::DGAccumulator<uint64_t>>();
@@ -67,7 +70,7 @@ struct Instrument {
     remote_comm_to_host =
         std::make_unique<std::unique_ptr<galois::DGAccumulator<uint64_t>[]>[]>(
             numH);
-    for (uint32_t i=0; i<numH; i++) {
+    for (uint32_t i = 0; i < numH; i++) {
       remote_read_to_host[i] =
           std::make_unique<galois::DGAccumulator<uint64_t>[]>(CACHE_SAMPLES + 1);
       remote_write_to_host[i] =
@@ -76,88 +79,7 @@ struct Instrument {
           std::make_unique<galois::DGAccumulator<uint64_t>[]>(CACHE_SAMPLES + 1);
     }
     clear();
-
-    // load transposed graph (to count incoming degrees)
-    std::vector<unsigned> _;
-    auto tgr =
-        loadDistGraph<typename Graph::GraphNode, typename Graph::EdgeType,
-                      /*iterateOutEdges*/ false>(_);
-
-    // in-degree counting (using transposed graph)
-    galois::InsertBag<std::pair<uint64_t, typename Graph::GraphNode>>
-        indeg_nodes;
-    const auto& allNodes = tgr->allNodesRange();
-    galois::do_all(
-        galois::iterate(allNodes),
-        [&](auto node) {
-          // ignore master nodes
-          if (tgr->isOwned(tgr->getGID(node))) {
-            return;
-          }
-          indeg_nodes.emplace(std::make_pair(
-              std::distance(tgr->edge_begin(node), tgr->edge_end(node)), node));
-        },
-        galois::steal(), galois::no_stats());
-
-    /**
-     * NOTE: alternative way to count incoming degree in original graph
-     *
-    constexpr uint64_t TEST_NODE = 45355;
-    std::atomic<uint64_t> indeg{0};
-    const auto& allNodes = graph->allNodesRange();
-    galois::do_all(
-        galois::iterate(allNodes),
-        [&](auto src) {
-          for (auto e : graph->edges(src)) {
-            auto node = graph->getEdgeDst(e);
-            if (graph->isOwned(graph->getGID(node))) {
-              continue;
-            }
-            if (node == TEST_NODE) {
-              galois::atomicAdd(indeg, 1ul);
-            }
-          }
-        },
-        galois::steal(), galois::no_stats());
-    assert(indeg.load(std::memory_order_relaxed) == 27);
-     */
-
-    // descending sort
-    std::multimap<uint64_t, typename Graph::GraphNode, std::greater<int>>
-        sorted_indeg_nodes(indeg_nodes.begin(), indeg_nodes.end());
-    // cut into levels
-    int64_t sorted_indeg_nodes_bound = sorted_indeg_nodes.size() * CACHE_BOUND / 100;
-    auto [level_size, surplus] =
-        std::div(sorted_indeg_nodes_bound, (int64_t)CACHE_SAMPLES);
-    auto it = sorted_indeg_nodes.begin();
-    auto end = std::next(it, sorted_indeg_nodes_bound);
-    for (int cache_level = 1; cache_level <= CACHE_SAMPLES; cache_level++) {
-      if (surplus) {
-        end = std::next(it, level_size + 1);
-        surplus--;
-      } else {
-        end = std::next(it, level_size);
-      }
-      galois::do_all(
-          galois::iterate(it, end),
-          [&](auto& deg_node) {
-            auto& [deg, node]      = deg_node;
-            node_cache_level[node] = cache_level;
-          },
-          galois::no_stats());
-      it = end;
-    }
     
-    it = std::next(sorted_indeg_nodes.begin(), sorted_indeg_nodes_bound);
-    end = sorted_indeg_nodes.end();
-    galois::do_all(
-        galois::iterate(it, end),
-        [&](auto& deg_node) {
-            auto& [deg, node]      = deg_node;
-            node_cache_level[node] = -1;
-        },
-        galois::no_stats());
-
     // start instrumentation
     file.open(graphName + "_" + std::to_string(numH) + "procs_id" + std::to_string(hid), std::ios::out | std::ios::trunc);
     file << "#####   Stat   #####" << std::endl;
@@ -176,17 +98,17 @@ struct Instrument {
     local_write_stream->reset();
     master_read->reset();
     master_write->reset();
-    for (auto i=0ul; i<CACHE_SAMPLES+1; i++) {
-        mirror_read[i].reset();
-        mirror_write[i].reset();
-        remote_read[i].reset();
-        remote_write[i].reset();
+    for (auto i = 0ul; i < CACHE_SAMPLES + 1; i++) {
+      mirror_read[i].reset();
+      mirror_write[i].reset();
+      remote_read[i].reset();
+      remote_write[i].reset();
     }
-    for (auto i=0ul; i<numHosts; i++) {
-        for (auto j=0ul; j<CACHE_SAMPLES+1; j++) {
-            remote_read_to_host[i][j].reset();
-            remote_write_to_host[i][j].reset();
-            remote_comm_to_host[i][j].reset();
+    for (auto i = 0ul; i < numHosts; i++) {
+      for (auto j = 0ul; j < CACHE_SAMPLES + 1; j++) {
+        remote_read_to_host[i][j].reset();
+        remote_write_to_host[i][j].reset();
+        remote_comm_to_host[i][j].reset();
       }
     }
 #endif
@@ -211,61 +133,156 @@ struct Instrument {
       *master_read += 1;
       return;
     }
-    // mirror
-    int cache_level = node_cache_level[node];
-
-    if (cache_level == -1) {
-        for (int i=0; i<CACHE_SAMPLES+1; i++) {
-          remote_read[i] += 1;
-          remote_read_to_host[graph->getHostID(gid)][i] += 1;
-        }
-    } else {
-        for (int i=0; i<cache_level; i++) { // different configs
-          remote_read[i] += 1;
-          remote_read_to_host[graph->getHostID(gid)][i] += 1;
-        }
-
-        for (auto i=cache_level; i<CACHE_SAMPLES+1; i++) {
-          mirror_read[i] += 1;
-        }
-    }
+    
+    auto& nodeData = graph->getData(node);
+    nodeData.read.fetch_add(1, std::memory_order_relaxed);
 #else
     (void) node;
 #endif
   }
 
-  void record_write_random(typename Graph::GraphNode node, bool comm) {
+  void record_write_random(typename Graph::GraphNode node) {
 #if GALOIS_INSTRUMENT
     auto gid = graph->getGID(node);
     if (graph->isOwned(gid)) { // master
       *master_write += 1;
       return;
     }
-    // mirror
-    int cache_level = node_cache_level[node];
-
-    if (cache_level == -1) {
-        for (int i=0; i<CACHE_SAMPLES+1; i++) {
-          remote_write[i] += 1;
-          remote_write_to_host[graph->getHostID(gid)][i] += 1;
-        }
-    } else {
-        for (int i=0; i<cache_level; i++) {
-          remote_write[i] += 1;
-          remote_write_to_host[graph->getHostID(gid)][i] += 1;
-        }
-
-        for (auto i=cache_level; i<CACHE_SAMPLES+1; i++) {
-          mirror_write[i] += 1;
-
-          if (comm) {
-            remote_comm_to_host[graph->getHostID(gid)][i] += 1;
-          }
-        }
-    }
+    
+    auto& nodeData = graph->getData(node);
+    nodeData.write.fetch_add(1, std::memory_order_relaxed);
 #else
     (void) node;
-    (void) comm;
+#endif
+  }
+
+  void update() {
+#if GALOIS_INSTRUMENT
+      form_vector();
+      count_access();
+#endif
+  }
+
+  void form_vector () {
+#if GALOIS_INSTRUMENT
+      auto mirrorSize = graph->numMirrors();
+      node_access.reserve(mirrorSize);
+
+      const auto& allMirrorNodes = graph->mirrorNodesRange();
+      galois::do_all(
+          galois::iterate(allMirrorNodes),
+          [&](auto node) {
+              auto node_GID = graph->getGID(node);
+              auto& nodeData = graph->getData(node);
+              std::vector<uint64_t> temp {graph->getHostID(node_GID), nodeData.read, nodeData.write, nodeData.read+nodeData.write};
+
+              vector_lock->lock();
+              node_access.push_back(temp);
+              vector_lock->unlock();
+          },
+          galois::steal(), galois::no_stats());
+#endif
+  }
+
+  void count_access() {
+#if GALOIS_INSTRUMENT
+      // sort the access vector first
+      std::sort(node_access.begin(), node_access.end(), sortAccess);
+
+      // count # of reads and writes
+      int vector_size = node_access.size();
+      int vector_bound = vector_size * CACHE_BOUND / 100;
+      unsigned chunk_size = vector_bound / CACHE_SAMPLES;
+      unsigned remainder = vector_bound % CACHE_SAMPLES;
+	
+      for (unsigned i=0; i<CACHE_SAMPLES; i++) {
+		  for (unsigned j=0; j<chunk_size; j++) {
+			  int index = i * chunk_size + j;
+			  uint64_t owner_host = node_access[index][0];
+			  uint64_t read = node_access[index][1];
+			  uint64_t write = node_access[index][2];
+
+              for (unsigned k=0; k<i+1; k++) {
+                  remote_read[k] += read;
+                  remote_read_to_host[owner_host][k] += read;
+
+                  remote_write[k] += write;
+                  remote_write_to_host[owner_host][k] += write;
+              }
+
+              for (unsigned k=i+1; k<CACHE_SAMPLES+1; k++) {
+
+                  mirror_read[k] += read;
+                  mirror_write[k] += write;
+/*                  
+                  if (read > 0) {
+                      mirror_read[k] += (read-1);
+                      remote_read[k] += 1;
+
+                      mirror_write[k] += write;
+                  } else {
+                      if (write > 0) {
+                          mirror_write[k] += (write-1);
+                          remote_write[k] += 1;
+                      }
+                  }
+*/                
+                  if (write > 0) {
+                      remote_comm_to_host[owner_host][k] += 1;
+                  }
+              }
+		  }
+	  }
+
+      for (unsigned i=0; i<remainder; i++) {
+          int index = CACHE_SAMPLES * chunk_size + i;
+          uint64_t owner_host = node_access[index][0];
+          uint64_t read = node_access[index][1];
+          uint64_t write = node_access[index][2];
+            
+          for (unsigned k=0; k<CACHE_SAMPLES; k++) {
+              remote_read[k] += read;
+              remote_read_to_host[owner_host][k] += read;
+
+              remote_write[k] += write;
+              remote_write_to_host[owner_host][k] += write;
+          }
+
+          mirror_read[CACHE_SAMPLES] += read;
+          mirror_write[CACHE_SAMPLES] += write;
+/*
+          if (read > 0) {
+              mirror_read[CACHE_SAMPLES] += (read-1);
+              remote_read[CACHE_SAMPLES] += 1;
+
+              mirror_write[CACHE_SAMPLES] += write;
+          } else {
+              if (write > 0) {
+                  mirror_write[CACHE_SAMPLES] += (write-1);
+                  remote_write[CACHE_SAMPLES] += 1;
+              }
+          }
+*/        
+          if (write > 0) {
+              remote_comm_to_host[owner_host][CACHE_SAMPLES] += 1;
+          }
+      }
+      
+      for (auto index=vector_bound; index<vector_size; index++) {
+          uint64_t owner_host = node_access[index][0];
+          uint64_t read = node_access[index][1];
+          uint64_t write = node_access[index][2];
+          
+          for (unsigned k=0; k<CACHE_SAMPLES+1; k++) {
+              remote_read[k] += read;
+              remote_read_to_host[owner_host][k] += read;
+
+              remote_write[k] += write;
+              remote_write_to_host[owner_host][k] += write;
+          }
+      }
+
+      node_access.clear();
 #endif
   }
 
@@ -293,7 +310,7 @@ struct Instrument {
     file << "host " << host_id
          << " master writes: " << master_write->read_local() << std::endl;
 
-    for (int i=0; i<CACHE_SAMPLES+1; i++) {
+    for (int i = 0; i < CACHE_SAMPLES+1; i++) {
       file << "host " << host_id << " cache " << CACHE_STEP*i
            << " mirror reads: " << mirror_read[i].read_local() << std::endl;
       file << "host " << host_id << " cache " << CACHE_STEP*i
@@ -303,7 +320,7 @@ struct Instrument {
       file << "host " << host_id << " cache " << CACHE_STEP*i
            << " remote writes: " << remote_write[i].read_local() << std::endl;
 
-      for (uint32_t j=0; j<num_hosts; j++) {
+      for (uint32_t j = 0; j < num_hosts; j++) {
         file << "host " << host_id << " cache " << CACHE_STEP*i << " remote read to host "
              << j << ": " << remote_read_to_host[j][i].read_local()
              << std::endl;
