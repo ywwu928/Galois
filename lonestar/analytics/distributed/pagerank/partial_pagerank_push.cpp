78bca1bb7278e7ace1f74b1380fe462d64011f36
ywwu928 <ywwu928@utexas.edu>
2025-04-01 15:56:39 -0500

    print out per round work items

--- lonestar/analytics/distributed/pagerank/partial_pagerank_push.cpp
+++ lonestar/analytics/distributed/pagerank/partial_pagerank_push.cpp
@@ -231,9 +231,7 @@ struct PageRank {
       
       _net.resetWorkTermination();
 
-      galois::runtime::reportStat_Tsum(
-          REGION_NAME_RUN.c_str(), "NumWorkItems_" + (syncSubstrate->get_run_identifier()),
-          (unsigned long)dga.read_local());
+      galois::runtime::reportStatCond_Single<USER_STATS>(REGION_NAME_RUN.c_str(), "NumWorkItems_Round_" + std::to_string(_num_iterations), (unsigned long)dga.read_local());
       
       if (dga.reduce(syncSubstrate->get_run_identifier()) == 0) {
           StatTimer_total.stop();
