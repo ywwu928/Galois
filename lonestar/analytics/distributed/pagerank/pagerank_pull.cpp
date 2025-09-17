e1d4523e91e83ef7c9123fadd4e884252503ebe1
ywwu928 <ywwu928@utexas.edu>
2025-09-17 13:58:12 -0500

    add timer for flushing remote work

--- lonestar/analytics/distributed/pagerank/pagerank_pull.cpp
+++ lonestar/analytics/distributed/pagerank/pagerank_pull.cpp
@@ -337,6 +337,8 @@ struct PageRankAll {
       galois::CondStatTimer<USER_STATS> StatTimer_delta(delta_str.c_str(), REGION_NAME_RUN.c_str());
       std::string compute_str("Compute_Round_" + std::to_string(_num_iterations));
       galois::CondStatTimer<USER_STATS> StatTimer_compute(compute_str.c_str(), REGION_NAME_RUN.c_str());
+      std::string flush_str("Flush_Round_" + std::to_string(_num_iterations));
+      galois::CondStatTimer<USER_STATS> StatTimer_flush(flush_str.c_str(), REGION_NAME_RUN.c_str());
       std::string comm_str("Communication_Round_" + std::to_string(_num_iterations));
       galois::CondStatTimer<USER_STATS> StatTimer_comm(comm_str.c_str(), REGION_NAME_RUN.c_str());
 
@@ -364,7 +366,9 @@ struct PageRankAll {
 
       // inform all other hosts that this host has finished sending messages
       // force all messages to be processed before continuing
+      StatTimer_flush.start();
       _net.flushRemoteWork();
+      StatTimer_flush.stop();
 
       StatTimer_comm.start();
       syncSubstrate->poll_for_remote_work<Reduce_add_residual>();
