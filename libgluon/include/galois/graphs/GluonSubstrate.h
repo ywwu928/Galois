3440f1aecca6e96a65059d1fc9390fcbc18b136a
ywwu928 <ywwu928@utexas.edu>
2025-09-24 12:37:37 -0500

    comment out number of messages and poll idle timer statistics

--- libgluon/include/galois/graphs/GluonSubstrate.h
+++ libgluon/include/galois/graphs/GluonSubstrate.h
@@ -1268,7 +1268,14 @@ public:
 
     template<typename FnTy>
     void poll_for_remote_work() {
+    //void poll_for_remote_work(std::string REGION_NAME_RUN, unsigned round) {
         std::atomic<bool> terminateFlag = false;
+
+        //galois::GAccumulator<uint32_t> numMsg;
+        //numMsg.reset();
+
+        //std::string idle_str("Poll_Idle_Timer_" + std::to_string(round));
+        //galois::StatTimer StatTimer_idle(idle_str.c_str(), REGION_NAME_RUN.c_str());
 
         galois::on_each(
             [&](unsigned, unsigned) {
@@ -1282,10 +1289,13 @@ public:
                 uint32_t lid;
                 ValTy val;
 
+                //StatTimer_idle.start();
                 while(!terminateFlag) {
                     success = net.receiveRemoteWork(terminateFlag, fullFlag, buf, bufLen);
 
                     if (success) { // received message
+                        //StatTimer_idle.stop();
+                        //numMsg  += 1;
                         if (fullFlag) {
                             msgCount = net.workCount;
                         }
@@ -1300,10 +1310,14 @@ public:
                         }
                         
                         net.deallocateRecvBuffer(buf);
+                        //StatTimer_idle.start();
                     }
                 }
+                //StatTimer_idle.stop();
             }
         );
+
+        //galois::runtime::reportStat_Single(REGION_NAME_RUN.c_str(), "NumMessages_Round_" + std::to_string(round), numMsg.reduce());
     }
 
 };
