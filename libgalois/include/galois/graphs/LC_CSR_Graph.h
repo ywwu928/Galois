ed3b26e93251b2a701c6ad129f4503b6be0ca773
ywwu928 <ywwu928@utexas.edu>
2025-09-23 22:26:03 -0500

    upgrade C++ standard from 17 to 20

--- libgalois/include/galois/graphs/LC_CSR_Graph.h
+++ libgalois/include/galois/graphs/LC_CSR_Graph.h
@@ -448,13 +448,13 @@ public:
 
   edge_iterator findEdge(GraphNode N1, GraphNode N2) {
     return std::find_if(edge_begin(N1), edge_end(N1),
-                        [=](edge_iterator e) { return getEdgeDst(e) == N2; });
+                        [=, this](edge_iterator e) { return getEdgeDst(e) == N2; });
   }
 
   edge_iterator findEdgeSortedByDst(GraphNode N1, GraphNode N2) {
     auto e = std::lower_bound(
         edge_begin(N1), edge_end(N1), N2,
-        [=](edge_iterator e, GraphNode N) { return getEdgeDst(e) < N; });
+        [this](edge_iterator e, GraphNode N) { return getEdgeDst(e) < N; });
     return (getEdgeDst(e) == N2) ? e : edge_end(N1);
   }
 
@@ -513,7 +513,7 @@ public:
   void sortAllEdgesByDst(MethodFlag mflag = MethodFlag::WRITE) {
     galois::do_all(
         galois::iterate(size_t{0}, this->actualSize()),
-        [=](GraphNode N) { this->sortEdgesByDst(N, mflag); },
+        [=, this](GraphNode N) { this->sortEdgesByDst(N, mflag); },
         galois::no_stats(), galois::steal());
   }
 
