b5b2fffde1ef951dd4420a9dac2f885edfa70e97
ywwu928 <ywwu928@utexas.edu>
2025-11-15 22:35:01 -0600

    no core pinning

--- libgalois/include/galois/substrate/ThreadPool.h
+++ libgalois/include/galois/substrate/ThreadPool.h
@@ -178,10 +178,10 @@ public:
   bool isRunning() const { return running; }
 
   //! return the number of non-reserved threads in the pool
-  unsigned getMaxUsableThreads() const { return mi.maxThreads - commThreadNum - reserved; }
+  unsigned getMaxUsableThreads() const { return mi.maxThreads - reserved; }
   //! return the number of threads supported by the thread pool on the current
   //! machine
-  unsigned getMaxThreads() const { return mi.maxThreads - commThreadNum; }
+  unsigned getMaxThreads() const { return mi.maxThreads; }
   unsigned getMaxCores() const { return mi.maxCores; }
   unsigned getMaxSockets() const { return mi.maxSockets; }
   unsigned getMaxNumaNodes() const { return mi.maxNumaNodes; }
